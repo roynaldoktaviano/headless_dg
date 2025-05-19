19f797da45fd483a02284dfa00f44c5fac570f6b
Roynald Oktaviano <roynald.okta@gmail.com>
2025-05-19 13:39:18 +0700

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-export default function RootLayout({children}: {children: React.ReactNode}) {
-    return (
-        <html lang="en">
-            <body>
-                <main>
-                    {children}
-                </main>
-            </body>
-        </html>
-    );
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <p>Test</p>
+    </div>
+  )
 }
