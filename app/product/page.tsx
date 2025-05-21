666926f1cda5ffa7933e1b09beffd2c99c497d28
Roynald Oktaviano <roynald.okta@gmail.com>
2025-05-21 13:44:00 +0700

    new

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -1,16 +1,27 @@
-// src/app/page.tsx (atau index.tsx)
-'use client';
+// // src/app/page.tsx (atau index.tsx)
+// 'use client';
 
-import { useProducts } from '@/hooks/useProduct';
-import ProductList from '../(components)/product/ProductList';
+// import { useProducts } from '@/hooks/useProduct';
+// import ProductList from '../(components)/product/ProductList';
 
-export default function Product() {
-  const { products, isLoading } = useProducts();
+// export default function Product() {
+//   const { products, isLoading } = useProducts();
 
+//   return (
+//     <main className="p-4">
+//       <h1 className="text-2xl font-bold mb-4">Katalog Produk</h1>
+//       {isLoading ? <p>Loading...</p> : <ProductList products={products} />}
+//     </main>
+//   );
+// }
+
+
+import React from 'react'
+
+export default function Produk() {
   return (
-    <main className="p-4">
-      <h1 className="text-2xl font-bold mb-4">Katalog Produk</h1>
-      {isLoading ? <p>Loading...</p> : <ProductList products={products} />}
-    </main>
-  );
+    <div>
+      
+    </div>
+  )
 }
