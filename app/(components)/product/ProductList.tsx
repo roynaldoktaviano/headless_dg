f6d8858141d8d8feff2803772f2d3f38f7f90532
Roynald Oktaviano <roynald.okta@gmail.com>
2025-05-21 13:41:26 +0700

    new

--- app/(components)/product/ProductList.tsx
+++ app/(components)/product/ProductList.tsx
@@ -1,12 +1,22 @@
-import ProductCard from "./ProductCard";
-import { Product } from "@/types/product";
+// import ProductCard from "./ProductCard";
+// import { Product } from "@/types/product";
 
-export default function ProductList({ products }: { products: Product[] }) {
+// export default function ProductList({ products }: { products: Product[] }) {
+//   return (
+//     <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
+//       {products.map(product => (
+//         <ProductCard key={product.id} product={product} />
+//       ))}
+//     </div>
+//   );
+// }
+
+import React from 'react'
+
+export default function ProductList() {
   return (
-    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
-      {products.map(product => (
-        <ProductCard key={product.id} product={product} />
-      ))}
+    <div>
+      
     </div>
-  );
+  )
 }
